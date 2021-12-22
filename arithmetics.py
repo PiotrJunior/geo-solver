de9e6d2710066c5806dcbab6e3590ded931d1c97
Piotr Maksymiuk <2003piotr@gmail.com>
2021-12-22 09:45:39 +0100

    Tests for Rational and beter init of classes

--- arithmetics.py
+++ arithmetics.py
@@ -5,24 +5,34 @@ from typing import Dict, List, Union
 class Monomial:
     """ Monomial class
     """
-    def __init__(self, factor : Union[float, int] = 0, terms : Dict[str, float] = {}) -> None:
-        """Init Monomial
-
-        Args:
-            factor (float | int, optional): Factor of the monomial. Defaults to 0.
-            terms (Dict[str, float], optional): Dict of terms in "var" : value format. Defaults to {}.
-        """
-        self.factor = factor
-        self.terms = terms.copy()
+    def __init__(self, factor = 0, terms = None) -> None:
+        if isinstance(terms, dict):
+            self.factor = factor
+            self.terms = terms.copy()
+        elif isinstance(terms, str):
+            self.factor = factor
+            self.terms = {terms: 1}        
+        elif terms is None:
+            if isinstance(factor, dict):
+                self.factor = 1
+                self.terms = factor.copy()
+            elif isinstance(factor, str):
+                self.factor = 1
+                self.terms = {factor:1}
+            elif isinstance(factor, (int, float)):
+                self.factor = factor
+                self.terms = {}
+            else:
+                raise Exception("Wrong arguments")
+        else:
+            raise Exception("Wrong arguments")    
     
     def __str__(self) -> str:
         return str(self.factor) + "".join([str(var)+"^"+str(power) for var, power in self.terms.items()])
 
     def __mul__(self, other : Union[Monomial, int, float]) -> Monomial:
-        if isinstance(other, int):
-            tmp = copy(self)
-            tmp.factor *= other
-            return tmp
+        if not isinstance(other, Monomial):
+            other = Monomial(other)
         factor = self.factor * other.factor
         terms = self.terms.copy()
         for var, power in other.terms.items():
@@ -33,20 +43,26 @@ class Monomial:
             if terms[var] == 0:
                 terms.pop(var)
         return Monomial(factor, terms)
-
+    
     def __add__(self, other : Monomial) -> Monomial:
+        if not isinstance(other, Monomial):
+            other = Monomial(other)
         if self.terms != other.terms:
             raise Exception("Monomials with diffrent terms")
         return Monomial(self.factor + other.factor, self.terms)
-
+    
     def __sub__(self, other : Monomial) -> Monomial:
+        if not isinstance(other, Monomial):
+            other = Monomial(other)
         if self.terms != other.terms:
             raise Exception("Monomials with diffrent terms")
         return Monomial(self.factor - other.factor, self.terms)
-
+    
     def __eq__(self, other : Monomial) -> bool:
+        if not isinstance(other, Monomial):
+            other = Monomial(other)
         return self.terms == other.terms
-
+    
     def same(self, other : Monomial) -> bool:
         """Checks if Monomials are the same including factor
 
@@ -56,17 +72,23 @@ class Monomial:
         Returns:
             bool: True when are the same
         """
+        if not isinstance(other, Monomial):
+            other = Monomial(other)
         return self.terms == other.terms and self.factor == other.factor
 
+    __rmul__ = __mul__
+    __radd__ = __add__
+    __rsub__ = __sub__
+    __req__ = __eq__
 
 class Polynomial:
     def __init__(self, terms : List[Monomial] = []) -> None:
-        """Init Polynomial
-
-        Args:
-            terms (List[Monomial], optional): List of Monomials. Defaults to [].
-        """
-        self.terms = terms.copy()
+        if isinstance(terms, list):
+            self.terms = [x if isinstance else Monomial(x) for x in terms]
+        elif isinstance(terms, Monomial):
+            self.terms = [terms]
+        else:
+            self.terms = [Monomial(terms)]
 
     def degree(self) -> int:
         """Returns degree (number of elements) of Polynomial
@@ -80,6 +102,8 @@ class Polynomial:
         return " + ".join([str(term) for term in self.terms])
 
     def __add__(self, other : Polynomial) -> Polynomial:
+        if not isinstance(other, Polynomial):
+            other = Polynomial(other)
         terms = self.terms.copy()
         for term in other.terms:
             if term in terms:
@@ -91,8 +115,10 @@ class Polynomial:
                 terms.append(term)
 
         return Polynomial(list(filter( lambda term : term.factor != 0, terms )))
-
+    
     def __sub__(self, other : Polynomial) -> Polynomial:
+        if not isinstance(other, Polynomial):
+            other = Polynomial(other)
         terms = self.terms.copy()
         for term in other.terms:
             if term in terms:
@@ -106,6 +132,8 @@ class Polynomial:
         return Polynomial(list(filter( lambda term : term.factor !=0, terms )))
 
     def __mul__(self, other : Polynomial) -> Polynomial:
+        if not isinstance(other, Polynomial):
+            other = Polynomial(other)
         poly = Polynomial()
         for term1 in self.terms:
             for term2 in other.terms:
@@ -113,10 +141,19 @@ class Polynomial:
 
         return poly
 
-    def __div__(self, other : Polynomial) -> Rational:
+    def __truediv__(self, other : Polynomial) -> Rational:
+        if not isinstance(other, Polynomial):
+            other = Polynomial(other)
         return Rational(self, other)
 
+    def __rtruediv__(self, other : Polynomial) -> Rational:
+        if not isinstance(other, Polynomial):
+            other = Polynomial(other)
+        return Rational(other, self)
+
     def __eq__(self, other : Polynomial) -> bool:
+        if not isinstance(other, Polynomial):
+            other = Polynomial(other)
         if self.degree() != other.degree():
             return False
         
@@ -125,17 +162,28 @@ class Polynomial:
                     return False
         return True
         
+    __radd__ = __add__
+    __rsub__ = __sub__
+    __rmul__ = __mul__
+    __req__ = __eq__
 
 class Rational:
-    def __init__(self, numerator : Polynomial, denominator : Polynomial) -> None:
+    def __init__(self, numerator : Polynomial, denominator : Polynomial = 1) -> None:
         """Init Rational function (Polynomial divided by Polynomial)
 
         Args:
             numerator (Polynomial): numerator of this Rational function
             denominator (Polynomial): denominator of this Rational function
         """
-        self.numerator = copy(numerator)
-        self.denominator = copy(denominator)
+        if isinstance(numerator, Polynomial):
+            self.numerator = copy(numerator)
+        else:
+            self.numerator = Polynomial(numerator)
+            
+        if isinstance(denominator, Polynomial):
+            self.denominator = copy(denominator)
+        else:
+            self.denominator = Polynomial(denominator)
 
     def __str__(self) -> str:
         num = str(self.numerator)
@@ -143,33 +191,55 @@ class Rational:
         return num + "\n" + "-" * max(len(num), len(den)) + "\n" + den
     
     def __mul__(self, other : Rational) -> Rational:
+        if not isinstance(other, Rational):
+            other = Rational(other)
         return Rational(
             self.numerator * other.numerator,
             self.denominator * other.denominator
         )
-
-    def __div__(self, other : Rational) -> Rational:
+    
+    def __truediv__(self, other : Rational) -> Rational:
+        if not isinstance(other, Rational):
+            other = Rational(other)
         return Rational(
             self.numerator * other.denominator,
             self.denominator * other.numerator
         )
 
+    def __rtruediv__(self, other : Rational) -> Rational:
+        if not isinstance(other, Rational):
+            other = Rational(other)
+        return Rational(
+            other.numerator   * self.denominator,
+            other.denominator * self.numerator
+        )
+
     def __add__(self, other : Rational) -> Rational:
+        if not isinstance(other, Rational):
+            other = Rational(other)
         return Rational(
             self.numerator * other.denominator + self.denominator * other.numerator,
             self.denominator * other.denominator
         )
 
     def __sub__(self, other : Rational) -> Rational:
+        if not isinstance(other, Rational):
+            other = Rational(other)
         return Rational(
             self.numerator * other.denominator - self.denominator * other.numerator,
             self.denominator * other.denominator
         )
 
     def __eq__(self, other : Rational) -> bool:
+        if not isinstance(other, Rational):
+            other = Rational(other)
         return self.numerator * other.denominator == self.denominator * other.numerator
 
-
+    __rmul__ = __mul__
+    __radd__ = __add__
+    __rsub__ = __sub__
+    __req__ = __eq__
+    
 # a = Rational( Polynomial([Monomial(1, {"a":1})]), Polynomial([Monomial(1, {})]) )
 # b = Rational( Polynomial([Monomial(1, {"b":1})]), Polynomial([Monomial(1, {})]) )
 # a = Polynomial([ Monomial(1, {"a":1}) ])
--- arithmetics_test.py
+++ arithmetics_test.py
@@ -28,11 +28,11 @@ class TestPolynomail(unittest.TestCase):
         p2 = Polynomial([m2])
         
         self.assertEqual(p1 + p1, Polynomial([ Monomial(4, {"a":3, "b":2}) ]))
-        self.assertEqual(p1 - p1, Polynomial([ ]))
+        self.assertEqual(p1 - p1, Polynomial())
         self.assertEqual(p1 + p2, Polynomial([m2, m1]))
         p1 += p2
-        self.assertEqual(p1 - p2, Polynomial([m1]))
-        self.assertEqual(p2 - p1, Polynomial([ m1*-1 ]))
+        self.assertEqual(p1 - p2, Polynomial(m1))
+        self.assertEqual(p2 - p1, Polynomial(m1*-1))
 
     def test_polynomial_mul(self):
         m1 = Monomial(2, {"a":3, "b":2})
@@ -43,6 +43,37 @@ class TestPolynomail(unittest.TestCase):
         self.assertEqual((p1 - p2) * (p1 + p2), Polynomial([ m1 * m1, m2 * m2 * Monomial(-1,{}) ]) )
         p3 = p1 + p2
         self.assertEqual( (p3 + p1) * p3, Polynomial([ m1*m1*2, m1*m2*3, m2*m2]))
+
+class TestRational(unittest.TestCase):
+    
+    def test_rational_mul(self):
+        m1 = Monomial(2, {"a":3, "b":2})
+        m2 = Monomial(3, {"b":3, "c":1})
+        m3 = Monomial(1, {"d":2, "e":1})
+        p1 = Polynomial([m1, m2])
+        p2 = Polynomial([m2, m3])
+        # r0 = Polynomial()
+        r1 = p1 / p2
+        r2 = Rational(p2, p1)
+        
+        self.assertNotEqual(r1, r2)
+        self.assertEqual(r1, 1/r2)
+        self.assertEqual(r1 * r1, r1 / r2)
+        
+    def test_rational_add(self):
+        m1 = Monomial(2, {"a":3, "b":2})
+        m2 = Monomial(3, {"b":3, "c":1})
+        m3 = Monomial(1, {"d":2, "e":1})
+        p1 = Polynomial([m1, m2])
+        p2 = Polynomial([m2, m3])
+        # r0 = Polynomial()
+        r1 = p1 / 1
+        r2 = Rational(p2)
+        
+        self.assertEqual(r1 + r2, Rational(p1+p2, 1))
+        r1 = p1 / p2
+        r2 = p2 / m3
+        self.assertEqual((r1 - r2) * (r1 + r2), r1*r1 - r2*r2)
 
 if __name__ == '__main__':
     unittest.main()
